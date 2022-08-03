20d0d373a07f7316c8224ec51e2d62922af3d097
fpekal <proxy154d@gmail.com>
2022-08-03 19:10:31 +0200

    Looby #1
    Added lobby
    Cleanup of code
    Infinite blocks in lobby
    Simple command to start a new game

--- code.lua
+++ code.lua
@@ -1,3 +1,5 @@
+local lobby_map = "<C><P H=\"800\" L=\"1600\" /><Z><S><S P=\"0,0,0.3,0.2,0,0,0,0\" L=\"3000\" o=\"3ba7df\" X=\"800\" c=\"4\" Y=\"399\" T=\"12\" H=\"3000\" /><S H=\"500\" L=\"191\" o=\"1b1103\" X=\"1269\" c=\"4\" Y=\"527\" T=\"12\" P=\"0,0,20,0.2,-2,0,0,0\" /><S L=\"3000\" H=\"3000\" X=\"829\" Y=\"2226\" T=\"6\" P=\"0,0,0.3,0.2,0,0,0,0\" /><S P=\"0,0,20,0.2,28,0,0,0\" L=\"250\" o=\"1b1103\" H=\"75\" c=\"4\" Y=\"484\" T=\"12\" X=\"1090\" /><S X=\"1147\" L=\"500\" o=\"1b1103\" H=\"75\" c=\"4\" Y=\"235\" T=\"12\" P=\"0,0,20,0.2,68,0,0,0\" /><S X=\"951\" L=\"250\" o=\"1b1103\" H=\"35\" c=\"4\" Y=\"363\" T=\"12\" P=\"0,0,20,0.2,48,0,0,0\" /><S P=\"0,0,20,0.2,-2,0,0,0\" L=\"250\" o=\"1b1103\" X=\"898\" c=\"4\" Y=\"444\" T=\"12\" H=\"35\" /><S P=\"0,0,20,0.2,118,0,0,0\" L=\"125\" o=\"1b1103\" X=\"1490\" c=\"4\" Y=\"505\" T=\"12\" H=\"10\" /><S H=\"10\" L=\"125\" o=\"1b1103\" X=\"834\" c=\"4\" Y=\"406\" T=\"12\" P=\"0,0,20,0.2,-42,0,0,0\" /><S P=\"0,0,20,0.2,28,0,0,0\" L=\"75\" o=\"1b1103\" H=\"10\" c=\"4\" Y=\"369\" T=\"12\" X=\"825\" /><S X=\"909\" L=\"75\" o=\"1b1103\" H=\"10\" c=\"4\" Y=\"205\" T=\"12\" P=\"0,0,20,0.2,8,0,0,0\" /><S P=\"0,0,20,0.2,-62,0,0,0\" L=\"75\" o=\"1b1103\" X=\"859\" c=\"4\" Y=\"250\" T=\"12\" H=\"10\" /><S X=\"1443\" L=\"250\" o=\"1b1103\" H=\"75\" c=\"4\" Y=\"352\" T=\"12\" P=\"0,0,20,0.2,-22,0,0,0\" /><S P=\"0,0,20,0.2,108,0,0,0\" L=\"500\" o=\"1b1103\" X=\"1362\" c=\"4\" Y=\"193\" T=\"12\" H=\"75\" /><S P=\"0,0,20,0.2,-12,0,0,0\" L=\"250\" o=\"1b1103\" H=\"35\" c=\"4\" Y=\"156\" T=\"12\" X=\"1467\" /><S H=\"35\" L=\"250\" o=\"1b1103\" X=\"1507\" c=\"4\" Y=\"438\" T=\"12\" P=\"0,0,20,0.2,-122,0,0,0\" /><S X=\"1001\" L=\"250\" o=\"1b1103\" H=\"35\" c=\"4\" Y=\"131\" T=\"12\" P=\"0,0,20,0.2,8,0,0,0\" /><S P=\"0,0,20,0.2,48,0,0,0\" L=\"250\" o=\"1b1103\" X=\"1244\" Y=\"148\" T=\"12\" H=\"35\" /><S H=\"10\" L=\"125\" o=\"1b1103\" X=\"1506\" c=\"4\" Y=\"101\" T=\"12\" P=\"0,0,20,0.2,118,0,0,0\" /><S P=\"0,0,20,0.2,158,0,0,0\" L=\"125\" o=\"1b1103\" H=\"10\" c=\"4\" Y=\"97\" T=\"12\" X=\"1273\" /><S P=\"0,0,20,0.2,78,0,0,0\" L=\"125\" o=\"1b1103\" H=\"10\" c=\"4\" Y=\"235\" T=\"12\" X=\"878\" /><S P=\"0,0,20,0.2,38,0,0,0\" L=\"75\" o=\"1b1103\" X=\"1261\" c=\"4\" Y=\"70\" T=\"12\" H=\"10\" /><S H=\"10\" L=\"75\" o=\"1b1103\" X=\"1553\" c=\"4\" Y=\"80\" T=\"12\" P=\"0,0,20,0.2,38,0,0,0\" /><S P=\"0,0,20,0.2,38,0,0,0\" L=\"75\" o=\"1b1103\" H=\"10\" c=\"4\" Y=\"559\" T=\"12\" X=\"1503\" /><S X=\"837\" L=\"125\" o=\"1b1103\" H=\"10\" c=\"4\" Y=\"135\" T=\"12\" P=\"0,0,20,0.2,158,0,0,0\" /><S H=\"10\" L=\"75\" o=\"1b1103\" X=\"824\" c=\"1\" Y=\"109\" T=\"12\" P=\"0,0,20,0.2,108,0,0,0\" /><S P=\"0,0,0.3,0.2,0,0,0,0\" L=\"211\" o=\"452c0a\" H=\"33\" Y=\"655\" T=\"12\" X=\"1154\" /><S L=\"211\" o=\"452c0a\" H=\"33\" X=\"1394\" Y=\"581\" T=\"12\" P=\"0,0,0.3,0.2,0,0,0,0\" /><S P=\"0,0,0.3,0.2,0,0,0,0\" L=\"211\" o=\"452c0a\" X=\"1092\" Y=\"538\" T=\"12\" H=\"33\" /><S L=\"211\" o=\"675c4e\" X=\"831\" H=\"33\" Y=\"425\" T=\"12\" P=\"0,0,0.3,0.2,0,0,0,0\" /><S P=\"0,0,0.3,0.2,0,0,0,0\" L=\"211\" o=\"675c4e\" H=\"33\" Y=\"474\" T=\"12\" X=\"1414\" /><S L=\"211\" o=\"452c0a\" H=\"33\" X=\"1176\" Y=\"373\" T=\"12\" P=\"0,0,0.3,0.2,0,0,0,0\" /><S P=\"0,0,0.3,0.2,0,0,0,0\" L=\"211\" o=\"675c4e\" X=\"1494\" Y=\"299\" T=\"12\" H=\"33\" /><S L=\"211\" o=\"452c0a\" X=\"919\" H=\"33\" Y=\"279\" T=\"12\" P=\"0,0,0.3,0.2,0,0,0,0\" /><S P=\"0,0,0.3,0.2,0,0,0,0\" L=\"211\" o=\"452c0a\" H=\"33\" Y=\"222\" T=\"12\" X=\"1239\" /><S L=\"211\" o=\"675c4e\" H=\"33\" X=\"941\" Y=\"144\" T=\"12\" P=\"0,0,0.3,0.2,0,0,0,0\" /><S P=\"0,0,0.3,0.2,0,0,0,0\" L=\"211\" o=\"452c0a\" X=\"1439\" Y=\"114\" T=\"12\" H=\"33\" /><S P=\"0,0,0.3,0.2,0,0,0,0\" L=\"450\" o=\"0\" X=\"356\" Y=\"366\" T=\"12\" H=\"585\" /><S L=\"430\" o=\"1d1d1d\" X=\"356\" H=\"565\" Y=\"366\" T=\"12\" P=\"0,0,0.3,0.2,0,0,0,0\" /></S><D><DS Y=\"710\" X=\"968\" /></D><O /></Z></C>"
+
 local maps = {
 	"@7912127",
 	"@7912131",
@@ -18,12 +20,19 @@ tfm.exec.disableAutoNewGame(true)
 tfm.exec.disableAutoTimeLeft(true)
 tfm.exec.disableAfkDeath(true)
 
-local leftBlocks = {}
+local players = {}
 
 ----------------------
 
+function configurePlayer(nick)
+	players[nick] = {
+		leftBlocks = 0,
+		infiniteBlocks = false
+	}
+end
+
 function resetBlocks(nick)
-	leftBlocks[nick] = startBlocks
+	players[nick].leftBlocks = startBlocks
 	destroyBlock(nick)
 	drawLeftBlocks(nick)
 	updatePlayersStats()
@@ -31,7 +40,7 @@ end
 
 -- How many blocks left. That text on the top-right
 function drawLeftBlocks(nick)
-	ui.addTextArea(0, "Objects left: "..leftBlocks[nick], nick, 680, 35, 200, 50, 0, 0, 0, true)
+	ui.addTextArea(0, "Objects left: "..players[nick].leftBlocks, nick, 680, 35, 200, 50, 0, 0, 0, true)
 end
 
 -- How many blocks each player has
@@ -43,8 +52,8 @@ end
 
 function updatePlayersStats()
 	local list = ""
-	for nick, blocks in pairs(leftBlocks) do
-		list = list..nick..": "..blocks.."\n"
+	for nick, player in pairs(players) do
+		list = list..nick..": "..player.leftBlocks.."\n"
 	end
 
 	ui.updateTextArea(1, list, nil)
@@ -57,7 +66,7 @@ end
 
 -- Does player has any blocks left?
 function hasBlocks(nick)
-	return leftBlocks[nick] > 0
+	return players[nick].leftBlocks > 0 or players[nick].infiniteBlocks
 end
 
 -- Place block
@@ -89,12 +98,24 @@ function startGame()
 
 	tfm.exec.newGame(newMap, isFlipped)
 
-	for nick in pairs(leftBlocks) do
+	for nick, player in pairs(players) do
+		player.infiniteBlocks = false
+	end
+
+	for nick in pairs(players) do
 		resetBlocks(nick)
 	end
 
 	tfm.exec.setGameTime(gameTime)
-	tfm.exec.setUIMapName("Czarodziejh <BL>- <I>Mouseour 1.1.0</BL></I>")
+	tfm.exec.setUIMapName("Czarodziejh <BL>- Mouseour <I>1.2.0</BL></I>")
+end
+
+function startLobby()
+	tfm.exec.newGame(lobby_map, false)
+
+	for nick, player in pairs(players) do
+		player.infiniteBlocks = true
+	end
 end
 
 -- Button showing stats of each player
@@ -104,17 +125,18 @@ function drawStatsButton(nick, event)
 end
 
 function drawStatsButtonOn(nick)
-	drawStatsButton(nick, "on")
+	drawStatsButton(nick, "statsOn")
 end
 
 function drawStatsButtonOff(nick)
-	drawStatsButton(nick, "off")
+	drawStatsButton(nick, "statsOff")
 end
 ----<
 
 -----------------------------
 
 function eventNewPlayer(nick)
+	configurePlayer(nick)
 	resetBlocks(nick)
 	drawLeftBlocks(nick)
 	drawStatsButtonOn(nick)
@@ -131,7 +153,10 @@ function eventMouse(nick, x, y)
 	if not hasBlocks(nick) then return end
 
 	-- Decrease amount of available blocks
-	leftBlocks[nick] = leftBlocks[nick] - 1
+	if players[nick].infiniteBlocks == false then
+		players[nick].leftBlocks = players[nick].leftBlocks - 1
+	end
+
 	spawnBlock(nick, x, y)
 
 	drawLeftBlocks(nick)
@@ -146,16 +171,30 @@ end
 
 
 function eventTextAreaCallback(id, nick, event)
-	if event == "on" then
+	if event == "statsOn" then
 		drawStatsButtonOff(nick)
 		showPlayersStats(nick)
-	elseif event == "off" then
+	elseif event == "statsOff" then
 		drawStatsButtonOn(nick)
 		hidePlayersStats(nick)
+	end
+end
+
+
+function eventPlayerWon(nick)
+	resetBlocks(nick)
+	tfm.exec.respawnPlayer(nick)
+end
+
+
+function eventChatCommand(nick, command)
+	if command == "startGame" then
+		startGame()
 	end
 end
 
 -----------------------------
 
 
-startGame()
+startLobby()
+--startGame()
